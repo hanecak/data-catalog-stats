b792f2e7083c227ce69cb7407f21d94cb51d8b8d
Peter Hanecak <hanecak@opendata.sk>
2015-08-26 17:07:19 +0200

    cz.ckan.net disabled as it no longer works

--- data_catalog_stats.py
+++ data_catalog_stats.py
@@ -59,9 +59,13 @@ DATA_CATALOGS = {
   #'data.gov.au': {
   #  'url': 'http://data.gov.au/api/'
   #},
-  'data.gov.cz': {
-    'url': 'http://cz.ckan.net/api/'
-  },
+  # As of now, http://cz.ckan.net/ does not work and http://data.gov.cz/
+  # redirects to http://portal.gov.cz/portal/obcan/, there we can find link
+  # to http://portal.gov.cz/portal/obcan/rejstriky/data/ but API ...  is
+  # nowhere to be found.  So, for now this is disabled.
+  #'data.gov.cz': {
+  #  'url': 'http://cz.ckan.net/api/'
+  #},
   'data.gov.ie': {
     'url': 'http://data.gov.ie/api/'
   },
