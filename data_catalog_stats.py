7d2da648c4124b0538612f03834f70c3ed02c7fb
Peter Hanecak <hanecak@opendata.sk>
2018-02-14 10:27:07 +0100

    minor clean-up: license statement fixed (this is actuall not part of ODN)

--- data_catalog_stats.py
+++ data_catalog_stats.py
@@ -2,20 +2,20 @@
 #
 # Copyright (C) 2011, 2013 - 2015 Peter Hanecak <hanecak@opendata.sk>
 #
-# This file is part of Open Data Node.
+# This file is part of data_catalog_stats.py.
 #
-# Open Data Node is free software: you can redistribute it and/or modify
+# data_catalog_stats.py is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Open Data Node is distributed in the hope that it will be useful,
+# data_catalog_stats.py is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Open Data Node.  If not, see <http://www.gnu.org/licenses/>.
+# along with data_catalog_stats.py. If not, see <http://www.gnu.org/licenses/>.
 #
 #
 # Simple stat gathering tool from some selected data catalogs.
